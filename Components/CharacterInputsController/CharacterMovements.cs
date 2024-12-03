309f55afad7d5fed8062f8329112f0b3012abea7
Noah <noah.gosciniak@epitech.eu>
2024-12-03 21:04:15 +0900

    add: mouse management

--- Components/CharacterInputsController/CharacterMovements.cs
+++ Components/CharacterInputsController/CharacterMovements.cs
@@ -1,60 +1,60 @@
 using EscapedfromTime.Components.CharacterAnimationsHandler;
 using Godot;
 
-namespace EscapedfromTime.Components.CharacterInputsController;
-
-[GlobalClass]
-public partial class CharacterMovements : Node
+namespace EscapedfromTime.Components.CharacterInputsController
 {
-	[Export] public CharacterBody3D Character;
-	[Export] public CharacterAnimations CharacterAnimations;
-	[Export] public SpringArm3D SpringArm;
-
-	[Export] public float Speed = 5.0f;
-	[Export] public float JumpVelocity = 4.5f;
-
-	public override void _PhysicsProcess(double delta)
+	[GlobalClass]
+	public partial class CharacterMovements : Node
 	{
-		Vector3 velocity = Character.Velocity;
+		[Export] public CharacterBody3D Character;
+		[Export] public CharacterAnimations CharacterAnimations;
+		[Export] public SpringArm3D SpringArm;
 
-		if (!Character.IsOnFloor())
-			velocity += Character.GetGravity() * (float)delta;
+		[Export] public float Speed = 5.0f;
+		[Export] public float JumpVelocity = 4.5f;
+		[Export] public float Gravity = -9.8f;
 
-		if (Input.IsActionJustPressed("ui_accept") && Character.IsOnFloor())
+		private Vector3 _velocity;
+		private bool _isJumping = false;
+
+		public override void _PhysicsProcess(double delta)
 		{
-			velocity.Y = JumpVelocity;
-			CharacterAnimations.Play(CharacterAnimation.Jump);
+			base._PhysicsProcess(delta);
+
+			Vector3 velocity = Character.Velocity;
+
+			if (!Character.IsOnFloor())
+			{
+				velocity.Y += Gravity * (float)delta;
+			}
+
+			if (Input.IsActionJustPressed("ui_accept") && Character.IsOnFloor())
+			{
+				velocity.Y = JumpVelocity;
+				CharacterAnimations.Play(CharacterAnimation.Jump);
+			}
+
+			Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+			if (inputDir != Vector2.Zero)
+			{
+				Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+				velocity.X = direction.X * Speed;
+				velocity.Z = direction.Z * Speed;
+
+				Character.LookAt(Character.GlobalTransform.Origin + direction, Vector3.Up);
+
+				CharacterAnimations.Play(CharacterAnimation.Walk);
+			}
+			else
+			{
+				velocity.X = Mathf.MoveToward(Character.Velocity.X, 0, Speed);
+				velocity.Z = Mathf.MoveToward(Character.Velocity.Z, 0, Speed);
+
+				CharacterAnimations.Play(CharacterAnimation.Idle);
+			}
+
+			Character.Velocity = velocity;
+			Character.MoveAndSlide();
 		}
-
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (inputDir != Vector2.Zero)
-		{
-			Vector3 forward = -SpringArm.GlobalTransform.Basis.Z;
-			forward.Y = 0;
-			forward = forward.Normalized();
-
-			Vector3 right = SpringArm.GlobalTransform.Basis.X;
-			right.Y = 0;
-			right = right.Normalized();
-
-			Vector3 direction = (right * inputDir.X + forward * inputDir.Y).Normalized();
-
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-
-			Character.LookAt(Character.GlobalTransform.Origin + direction, Vector3.Up);
-
-			CharacterAnimations.Play(CharacterAnimation.Walk);
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Character.Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Character.Velocity.Z, 0, Speed);
-
-			CharacterAnimations.Play(CharacterAnimation.Idle);
-		}
-
-		Character.Velocity = velocity;
-		Character.MoveAndSlide();
 	}
 }
--- Objects/TopDownCamera/TopDownCamera.cs
+++ Objects/TopDownCamera/TopDownCamera.cs
@@ -1,7 +1,41 @@
 using Godot;
 
-namespace EscapedfromTime.Objects.TopDownCamera;
-
-public partial class TopDownCamera : SpringArm3D
+namespace EscapedfromTime.Objects.TopDownCamera
 {
+	public partial class TopDownCamera : Node3D
+	{
+		[Export] public float Sensitivity = 1f;
+		[Export] public float MaxRotationX = 45f;
+		[Export] public float MinRotationX = -45f;
+		[Export] public float MaxRotationY = 45f;
+		[Export] public float MinRotationY = -45f;
+		[Export] public CharacterBody3D Character;
+		[Export] public float Distance = 10f;
+
+		private Vector2 _lastMousePosition;
+		private Vector2 _rotation;
+
+		public override void _Ready()
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+
+		public override void _Process(double delta)
+		{
+			Vector2 mouseDelta = Input.GetLastMouseVelocity();
+
+			_rotation.X -= mouseDelta.Y * (Sensitivity / 1000.0f);
+			_rotation.Y -= mouseDelta.X * (Sensitivity / 1000.0f);
+
+			// _rotation.X = Mathf.Clamp(_rotation.X, MinRotationX, MaxRotationX);
+			// _rotation.Y = Mathf.Clamp(_rotation.Y, MinRotationY, MaxRotationY);
+
+			Vector3 targetPosition = Character.GlobalPosition;
+			Vector3 offset = new Vector3(0, 0, Distance);
+			offset = offset.Rotated(Vector3.Up, Mathf.DegToRad(_rotation.Y));
+			offset = offset.Rotated(Vector3.Left, Mathf.DegToRad(_rotation.X));
+			GlobalPosition = targetPosition + offset;
+			LookAt(targetPosition, Vector3.Up);
+		}
+	}
 }
