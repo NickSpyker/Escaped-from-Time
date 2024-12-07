dd0ddf1c9902e1ced457b1eda92f921df0406e7e
Nicolas Spijkerman <nicospijke@gmail.com>
2024-12-07 12:55:35 +0900

    Fix movable cube bug

--- Components/PhysicsBehaviors/CanMoveRigidBody.cs
+++ Components/PhysicsBehaviors/CanMoveRigidBody.cs
@@ -1,3 +1,5 @@
+using System;
+using EscapedfromTime.Objects.TimePhysicsObjects;
 using Godot;
 
 namespace EscapedfromTime.Components.PhysicsBehaviors;
@@ -9,20 +11,26 @@ public partial class CanMoveRigidBody : Node
     [Export] public CharacterBody3D Character;
     [Export] public float CharacterMassInKg = 70f;
 
-    private Vector3 _velocity = Vector3.Zero;
-
     public override void _PhysicsProcess(double delta)
     {
-        if (Character.Velocity != Vector3.Zero)
-            _velocity = Character.Velocity * CharacterMassInKg;
-
         for (int i = 0; i < Character.GetSlideCollisionCount(); i++)
         {
             KinematicCollision3D collision = Character.GetSlideCollision(i);
-            if (collision.GetCollider() is RigidBody3D rigidBody)
-            {
-                rigidBody.ApplyForce(-collision.GetNormal() * _velocity);
-            }
+            if (collision.GetCollider() is not TimeMovableRigidBody rigidBody) continue;
+
+            Vector3 normal = collision.GetNormal();
+
+            const float upThreshold = 0.9f;
+            if (normal.Dot(Vector3.Up) > upThreshold) continue;
+
+            Vector3 pushDirection = _getPlanSignVector(-normal);
+
+            rigidBody.ApplyForce(pushDirection * CharacterMassInKg);
         }
+    }
+
+    private Vector3 _getPlanSignVector(Vector3 vector)
+    {
+        return new Vector3(Math.Sign(vector.X), 0, Math.Sign(vector.Z));
     }
 }
---
+++ Objects/TimePhysicsObjects/TimeMovableRigidBody.cs
@@ -0,0 +1,66 @@
+using System;
+using EscapedfromTime.Components.TimeTravelHandler;
+using Godot;
+
+namespace EscapedfromTime.Objects.TimePhysicsObjects;
+
+[GlobalClass]
+public partial class TimeMovableRigidBody : RigidBody3D
+{
+    [ExportCategory("Object Properties")]
+    [Export] public float MaxSpeed = 5.0f;
+    [Export] public float MaxForce = 100.0f;
+
+    private TimeMechanicsArea _timeMechanicsArea;
+    private bool _resetAll;
+
+    private Vector3 _initialPosition;
+    private Vector3 _initialRotation;
+
+    public override void _Ready()
+    {
+        Node currentParentNode = GetParent();
+
+        while (currentParentNode != null)
+        {
+            if (currentParentNode is TimeMechanicsArea timeLineArea)
+            {
+                _timeMechanicsArea = timeLineArea;
+                _timeMechanicsArea.ReStartTime += _onTimeRestart;
+                _initialPosition = GlobalPosition;
+                _initialRotation = GlobalRotation;
+                return;
+            }
+
+            currentParentNode = currentParentNode.GetParent();
+        }
+
+        GD.PrintErr("No TimeLineArea parent found for InteractableEntityRecorderPlayer");
+        throw new InvalidOperationException("InteractableEntityRecorderPlayer must be a child of a TimeLineArea node. Current parent hierarchy does not contain TimeLineArea.");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if (_resetAll)
+        {
+            ApplyForce(Vector3.Zero);
+            LinearVelocity = Vector3.Zero;
+            AngularVelocity = Vector3.Zero;
+            GlobalPosition = _initialPosition;
+            GlobalRotation = _initialRotation;
+            _resetAll = false;
+            return;
+        }
+
+        if (LinearVelocity.Length() > MaxSpeed)
+            LinearVelocity = LinearVelocity.Normalized() * MaxSpeed;
+
+        if (ConstantForce.Length() > MaxForce)
+            ConstantForce = ConstantForce.Normalized() * MaxForce;
+    }
+
+    private void _onTimeRestart()
+    {
+        _resetAll = true;
+    }
+}
