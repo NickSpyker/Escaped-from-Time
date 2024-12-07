7894ed8a17d403086996744941ffb614b77468eb
Nicolas Spijkerman <nicospijke@gmail.com>
2024-12-07 15:09:08 +0900

    Add interaction inputs and outputs

--- Objects/InteractableEntities/Inputs/InteractArea.cs
+++ Objects/InteractableEntities/Inputs/InteractArea.cs
@@ -4,50 +4,9 @@ namespace EscapedfromTime.Objects.InteractableEntities.Inputs;
 
 public partial class InteractArea : InputInteractableEntity
 {
-	[Export] public Label3D Label = null!;
-	[Export] public bool ShowLabel = true!;
-
-	private bool _canInteract;
-	private bool _isActionInProgress;
-
-	public override void _Ready()
-	{
-		Label.SetVisible(false);
-	}
-
-	public override void _Input(InputEvent @event)
-	{
-		if (!_canInteract) return;
-
-		if (_isActionInProgress && @event.IsActionReleased("player_interacts"))
-		{
-			_isActionInProgress = false;
-			if (ShowLabel) Label.SetText("INTERACTION_MESSAGE_LABEL");
-			return;
-		}
-		if (_isActionInProgress || !@event.IsActionPressed("player_interacts")) return;
-
-		_isActionInProgress = true;
-		if (ShowLabel) Label.SetText("INTERACTION_IN_PROGRESS");
-
-		InteractEmitSignal(InputInteractableEntity.SignalName.PlayerInteracted);
-	}
-
 	/* Signal */ public void OnBodyEntered(Node body)
 	{
 		if (!body.IsInGroup("player")) return;
-		Label.SetVisible(ShowLabel);
-		_isActionInProgress = false;
-		_canInteract = true;
-		if (ShowLabel) Label.SetText("INTERACTION_MESSAGE_LABEL");
-	}
-
-	/* Signal */ public void OnBodyExited(Node body)
-	{
-		if (!body.IsInGroup("player")) return;
-		Label.SetVisible(false);
-		_isActionInProgress = false;
-		_canInteract = false;
-		if (ShowLabel) Label.SetText("INTERACTION_MESSAGE_LABEL");
+		InteractEmitSignal(InputInteractableEntity.SignalName.PlayerInteracted);
 	}
 }
--- Objects/TimePhysicsObjects/TimeMovableRigidBody.cs
+++ Objects/TimePhysicsObjects/TimeMovableRigidBody.cs
@@ -7,60 +7,60 @@ namespace EscapedfromTime.Objects.TimePhysicsObjects;
 [GlobalClass]
 public partial class TimeMovableRigidBody : RigidBody3D
 {
-    [ExportCategory("Object Properties")]
-    [Export] public float MaxSpeed = 5.0f;
-    [Export] public float MaxForce = 100.0f;
+	[ExportCategory("Object Properties")]
+	[Export] public float MaxSpeed = 5.0f;
+	[Export] public float MaxForce = 100.0f;
 
-    private TimeMechanicsArea _timeMechanicsArea;
-    private bool _resetAll;
+	private TimeMechanicsArea _timeMechanicsArea;
+	private bool _resetAll;
 
-    private Vector3 _initialPosition;
-    private Vector3 _initialRotation;
+	private Vector3 _initialPosition;
+	private Vector3 _initialRotation;
 
-    public override void _Ready()
-    {
-        Node currentParentNode = GetParent();
+	public override void _Ready()
+	{
+		Node currentParentNode = GetParent();
 
-        while (currentParentNode != null)
-        {
-            if (currentParentNode is TimeMechanicsArea timeLineArea)
-            {
-                _timeMechanicsArea = timeLineArea;
-                _timeMechanicsArea.ReStartTime += _onTimeRestart;
-                _initialPosition = GlobalPosition;
-                _initialRotation = GlobalRotation;
-                return;
-            }
+		while (currentParentNode != null)
+		{
+			if (currentParentNode is TimeMechanicsArea timeLineArea)
+			{
+				_timeMechanicsArea = timeLineArea;
+				_timeMechanicsArea.ReStartTime += _onTimeRestart;
+				_initialPosition = GlobalPosition;
+				_initialRotation = GlobalRotation;
+				return;
+			}
 
-            currentParentNode = currentParentNode.GetParent();
-        }
+			currentParentNode = currentParentNode.GetParent();
+		}
 
-        GD.PrintErr("No TimeLineArea parent found for InteractableEntityRecorderPlayer");
-        throw new InvalidOperationException("InteractableEntityRecorderPlayer must be a child of a TimeLineArea node. Current parent hierarchy does not contain TimeLineArea.");
-    }
+		GD.PrintErr("No TimeLineArea parent found for InteractableEntityRecorderPlayer");
+		throw new InvalidOperationException("InteractableEntityRecorderPlayer must be a child of a TimeLineArea node. Current parent hierarchy does not contain TimeLineArea.");
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (_resetAll)
-        {
-            ApplyForce(Vector3.Zero);
-            LinearVelocity = Vector3.Zero;
-            AngularVelocity = Vector3.Zero;
-            GlobalPosition = _initialPosition;
-            GlobalRotation = _initialRotation;
-            _resetAll = false;
-            return;
-        }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_resetAll)
+		{
+			ApplyForce(Vector3.Zero);
+			LinearVelocity = Vector3.Zero;
+			AngularVelocity = Vector3.Zero;
+			GlobalPosition = _initialPosition;
+			GlobalRotation = _initialRotation;
+			_resetAll = false;
+			return;
+		}
 
-        if (LinearVelocity.Length() > MaxSpeed)
-            LinearVelocity = LinearVelocity.Normalized() * MaxSpeed;
+		if (LinearVelocity.Length() > MaxSpeed)
+			LinearVelocity = LinearVelocity.Normalized() * MaxSpeed;
 
-        if (ConstantForce.Length() > MaxForce)
-            ConstantForce = ConstantForce.Normalized() * MaxForce;
-    }
+		if (ConstantForce.Length() > MaxForce)
+			ConstantForce = ConstantForce.Normalized() * MaxForce;
+	}
 
-    private void _onTimeRestart()
-    {
-        _resetAll = true;
-    }
+	private void _onTimeRestart()
+	{
+		_resetAll = true;
+	}
 }
