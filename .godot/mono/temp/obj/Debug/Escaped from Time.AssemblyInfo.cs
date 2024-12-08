cf7e93a83cbf75c9207924472baaca7991e27814
Nicolas Spijkerman <nicospijke@gmail.com>
2024-12-08 14:36:31 +0900

    Add props to tutorial dungeon

--- .godot/mono/temp/obj/Debug/Escaped from Time.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Escaped from Time.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Escaped from Time")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68596dd3466775d569fe687d4574f178e4078d4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f892c0d90bfacfcb55cffb7e9231b2a2560f657")]
 [assembly: System.Reflection.AssemblyProductAttribute("Escaped from Time")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Escaped from Time")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
