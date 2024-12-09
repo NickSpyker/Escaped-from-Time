bce3f198fd7087b0b0e7debe86ba2df60c10b47b
Nicolas Spijkerman <nicospijke@gmail.com>
2024-12-09 22:56:43 +0900

    Add some languages

--- Scenes/SettingsMenu/SettingsMenu.cs
+++ Scenes/SettingsMenu/SettingsMenu.cs
@@ -9,10 +9,34 @@ public partial class SettingsMenu : Control
 		string optionLangNodePath = "MarginContainer/TabContainer/SETTINGS_TAB_GENERAL/VBoxContainer/GridContainer/OptionButtonLanguage";
 		OptionButton languageOption = GetNode<OptionButton>(optionLangNodePath);
 
-		languageOption.Selected = TranslationServer.GetLocale() switch { "fr_FR" => 1, "ko_KO" => 2, _ => 0 };
+		languageOption.Selected = TranslationServer.GetLocale() switch
+		{
+			"fr_FR" => 1,
+			"en_US" => 2,
+			"it_IT" => 3,
+			"de_DE" => 4,
+			"ko_KR" => 5,
+			"ja_JP" => 6,
+			"zh_CN" => 7,
+			"uk_UA" => 8,
+			"ru_RU" => 9,
+			_ => 0
+		};
 	}
 
 	public void OnSettingsClose() => SetVisible(false);
 
-	public void OnLanguageChanged(int i) => TranslationServer.SetLocale(i switch { 1 => "fr", 2 => "ko", _ => "en" });
+	public void OnLanguageChanged(int i) => TranslationServer.SetLocale(i switch
+	{
+		1 => "fr",
+		2 => "es",
+		3 => "it",
+		4 => "de",
+		5 => "ko",
+		6 => "ja",
+		7 => "zh",
+		8 => "uk",
+		9 => "ru",
+		_ => "en"
+	});
 }
